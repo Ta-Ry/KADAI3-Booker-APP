ac32c39c3824ad2c00eefb6f6110e19d74ed4b42
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-03-21 18:14:32 +0900

    [Add]応用課題6_非同期通信

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,15 @@
 class PostCommentsController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
-    	comment = current_user.post_comments.new(post_comment_params)
-    	comment.book_id = book.id
-    	comment.save
-    	redirect_to request.referrer || root_path
+		@book = Book.find(params[:book_id])
+		@post_comment = PostComment.new
+    	@comment = current_user.post_comments.new(post_comment_params)
+    	@comment.book_id = @book.id
+    	@comment.save
 	end
 	def destroy
-		book = Book.find(params[:book_id])
-        comment = current_user.post_comments.find_by(book_id: book.id)
-        comment.destroy
-    	redirect_to request.referrer || root_path
+		@book = Book.find(params[:book_id])
+        @comment = PostComment.find(params[:id])
+        @comment.destroy
 	end
 
 	private
